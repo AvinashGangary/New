a0673621b08f7f78eb4de2935bd645c77e389874
AvinashGangary <avinashgangary78@gmail.com>
2025-01-29 20:09:46 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -13,7 +13,7 @@ from threading import Thread
 
 loop = asyncio.get_event_loop()
 
-TOKEN = '7477846047:AAFWNH4jsq5GwIuPll6dh5Nq35kKK41m_c0'
+TOKEN = '7477846047:AAHfhpzPh92zALOLGrUQen7hvAxfnyNKetI'
 MONGO_URI = 'mongodb+srv://rishi:ipxkingyt@rishiv.ncljp.mongodb.net/?retryWrites=true&w=majority&appName=rishiv'
 FORWARD_CHANNEL_ID = -1002403226371
 CHANNEL_ID = -1002403226371
