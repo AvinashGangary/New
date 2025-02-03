298eb556204c93009a88b4432dcb62b8e142c395
AvinashGangary <avinashgangary78@gmail.com>
2025-02-04 02:02:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY 
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7228139123:AAGn3DnpneMgZ8q2uGQyqzmZZbWn01iC424")
+BOT_TOKEN = 7228139123:AAGn3DnpneMgZ8q2uGQyqzmZZbWn01iC424
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("KINGxVIPER")
+OWNER_USERNAME = KINGxVIPER
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("6824982812")
+ADMIN_IDS = 6824982812
 
 
 
